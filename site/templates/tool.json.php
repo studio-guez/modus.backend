329cf2090c73b4bac219bdaa7efad1bdae16f21d
OctoY <yann@octoplus.solutions>
2026-01-30 13:47:07 +0100

    feat: add tool and toolkit blueprints and templates with JSON handling

---
+++ site/templates/tool.json.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once '_utils/Utils.php';
+
+use Kirby\Cms\App;
+use Kirby\Cms\Page;
+use Kirby\Cms\Site;
+
+/** @global Kirby\Cms\App $kirby */
+/** @global Kirby\Cms\Site $site */
+/** @global Kirby\Cms\Page $page */
+
+$json = [];
+
+$body = $page->body()->toBlocks()->map(function ($item) {
+
+  $content = $item->toArray();
+
+  return [
+    'image'     => array_values(Utils::getImageArrayDataInPage($item->image()->toFiles())),
+    'content'   => $content,
+  ];
+})->data();
+
+$json['options'] = [
+  'headerTitle'        => $page->headerTitle()->value(),
+  'preview'            => $page->preview()->value(),
+  'headerImage'        => $page->headerImage()->toFile() ? Utils::getJsonEncodeImageData($page->headerImage()->toFile()) : null,
+  'dateStart'          => $page->dateStart()->value(),
+  'isExternalLink'     => $page->isExternalLink()->toBool(),
+  'externalUrl'        => $page->externalUrl()->value(),
+];
+
+$json['body'] = $body;
+$json['title'] = $page->title();
+
+echo json_encode($json);
---
+++ site/templates/toolkit.json.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once '_utils/Utils.php';
+
+use Kirby\Cms\App;
+use Kirby\Cms\Page;
+use Kirby\Cms\Site;
+
+/** @global Kirby\Cms\App $kirby */
+/** @global Kirby\Cms\Site $site */
+/** @global Kirby\Cms\Page $page */
+
+$json = [];
+
+$children = $page->children()->listed()->sort()->sortBy('dateStart')->map(function ($item) {
+
+  $content = $item->content();
+
+  return [
+    'headerImage' => array_values(Utils::getImageArrayDataInPage($content->headerimage()->toFiles())),
+    'slug'        => $item->slug(),
+    'content'     => $content->toArray(),
+  ];
+})->data();
+
+$json['options'] = [
+  'showInNav'       => $page->showMenu()->toBool(),
+  'headerTitle'     => $page->headerTitle()->value(),
+  'headerImage'     => $page->headerImage()->toFile() ? Utils::getJsonEncodeImageData($page->headerImage()->toFile()) : null,
+];
+
+$json['children'] = $children;
+
+echo json_encode($json);
