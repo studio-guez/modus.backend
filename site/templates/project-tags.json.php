baba9b602568f40d0c0fb01951035fcc6ad97579
OctoY <yann@octoplus.solutions>
2026-02-13 13:32:14 +0100

    style: format code indentation and spacing in project-tags.json.php

--- site/templates/project-tags.json.php
+++ site/templates/project-tags.json.php
@@ -13,17 +13,17 @@ $allTags = [];
 // Collect tags from all listed projects
 $projectsPage = $site->find('projects');
 if ($projectsPage) {
-    foreach ($projectsPage->children()->listed() as $item) {
-        $tagsValue = $item->content()->tags()->value();
-        if (!$tagsValue) continue;
-        
-        $resolvedTags = Utils::resolveTagsFromUuids($tagsValue, $site);
-        $allTags[] = $resolvedTags;
-    }
+  foreach ($projectsPage->children()->listed() as $item) {
+    $tagsValue = $item->content()->tags()->value();
+    if (!$tagsValue) continue;
+
+    $resolvedTags = Utils::resolveTagsFromUuids($tagsValue, $site);
+    $allTags[] = $resolvedTags;
+  }
 }
 
 $json = [
-    'tags' => Utils::collectUniqueTags($allTags),
+  'tags' => Utils::collectUniqueTags($allTags),
 ];
 
 echo json_encode($json);
