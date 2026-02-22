e1a14c9dc1d15156078b9429c438225bfb3d8890
OctoY <yann@octoplus.solutions>
2026-02-22 13:51:46 +0100

    feat: update home blueprint and template to include additional actualite fields

--- site/templates/home.json.php
+++ site/templates/home.json.php
@@ -32,6 +32,10 @@ $body = $page->body()->toBlocks()->map(function ($item) use ($site) {
 $json['options'] = [
   'headerTitle'     => $page->headerTitle()->value(),
   'headerImage'     => $page->headerImage()->toFile() ? Utils::getJsonEncodeImageData($page->headerImage()->toFile()) : null,
+  'actualite1title' => $page->actualite1title()->value() ?: null,
+  'actualite1link'  => $page->actualite1link()->value() ?: null,
+  'actualite2title' => $page->actualite2title()->value() ?: null,
+  'actualite2link'  => $page->actualite2link()->value() ?: null,
 ];
 
 $json['body'] = $body;
